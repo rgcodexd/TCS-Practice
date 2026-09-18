f1ead25d25bed92f2666dcd3c27f31ab36e2a518
Rahul Kumar Gupta <162043229+rgcodexd@users.noreply.github.com>
2026-09-18 12:10:07 +0530

    find prime

---
+++ Day0/array/findprime.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findprime(int n){
+    if (n<=1) return 0;
+
+    for(int i=2; i*i<=n;i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(){
+    int a;
+    cout<<"write the sacred no\n";
+    cin>>a;
+    if(findprime(a)){
+        cout<<"number is pure\n";
+    }
+    else cout<< "not pure\n";
+}
