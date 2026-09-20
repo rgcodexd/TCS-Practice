cf6562ec204e96db7a2e3c52a89e6446eb57cd20
Rahul Kumar Gupta <162043229+rgcodexd@users.noreply.github.com>
2026-09-21 00:01:34 +0530

    Day3 of coding

--- Day0/array/rotatedbelt.cpp
+++ Day0/array/rotatedbelt.cpp
@@ -1,1 +1,17 @@
-#include<bits/stdc
+#include<bits/stdc++.h>
+using namespace std;
+
+calss solution{
+    public:
+    //clcb- chrono locked conveyer belt problem
+    int clcb(vector<int>&arr){
+        int count=0;
+        int n=arr.size();
+
+        for (int i=0;i<n;i++){
+            if(arr[i]>arr[i+1]%n){
+                
+            }
+        }
+    }
+};
---
+++ Day1/findpaths.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    int findpaths(){
+        
+    }
+};
